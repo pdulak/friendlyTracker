3851bcf4a94bb7073b49b6137b0eb36e841d15b2
Pawel Dulak <pawel.dulak@gmail.com>
2018-02-08 00:22:51 +0100

    Playing with routing, errors and styling

--- routes/siteAdmin.js
+++ routes/siteAdmin.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('This will handle Admin interface');
 });
 
+router.get('/thisIsNotWorking', function(req, res, next) {
+  var a;
+  a = b;
+  res.send('This should not work');
+});
+
 module.exports = router;
