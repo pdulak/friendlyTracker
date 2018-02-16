1a715579e48c93f30fd151ba789871a0772e1fde
Pawel Dulak <pawel.dulak@gmail.com>
2018-02-16 22:45:17 +0100

    Added Mocha based testing and first dummy test

---
+++ test/index.js
@@ -0,0 +1,18 @@
+// This is just for organisation and reporting
+describe('Our application', function() {
+
+  // This is the name of the test
+  it('should understand basic mathematical principles', function(done) {
+
+    // We want this test to pass.
+    if (5 == 5) {
+      // If the behavior is as expected, call done with no argument.
+      done();
+    } else {
+      // Otherwise, call done with an error.
+      done(new Error("Not sure what's happened."));
+    }
+
+  });
+
+});
